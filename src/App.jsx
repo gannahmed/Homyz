f2fdb1a859d816c308393b25e34f720ff67317ea
ganna <gannahhmed551@gmail.com>
2023-07-20 18:28:26 +0300

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-
+import Header from "./component/Header/Header";
+import Hero from "./component/Hero/Hero";
+import "./App.css";
+import Companies from "./component/companies/Companies";
+import Residcencies from "./component/Residencies/Residcencies";
+import Value from "./component/Value/Value";
+import Contact from "./component/Contact/Contact";
+import GetStarted from "./component/GetStarted/GetStarted";
+import Footer from "./component/Footer/Footer";
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <Header />
+      <Hero />
+      <Companies />
+      <Residcencies />
+      <Value />
+      <Contact/>
+      <GetStarted/>
+      <Footer />
+    </div>
   );
 }
 
